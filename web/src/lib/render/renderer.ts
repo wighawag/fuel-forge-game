ba172cfe9f65ff673208edd10f27f78b0d887f7d
wighawag <wighawag@gmail.com>
2025-08-25 08:30:13 +0530

    use blockie

--- web/src/lib/render/renderer.ts
+++ web/src/lib/render/renderer.ts
@@ -1,23 +1,31 @@
 import type { OnchainState } from '$lib/onchain/types';
+import { Blockie } from '$lib/utils/ethereum/blockie';
 import { viewState } from '$lib/view';
-import { Graphics, type Container } from 'pixi.js';
+import { Container, Graphics, Sprite } from 'pixi.js';
 import type { Readable } from 'svelte/store';
+import { LoadingSprite } from './LoadingSprite';
 
 export function createRenderer(viewState: Readable<OnchainState>) {
-	let charactersDisplayObjects: { [id: string]: Graphics } = {};
+	let charactersDisplayObjects: { [id: string]: Container } = {};
 	let unsubscribe: (() => void) | undefined = undefined;
 	function onAppStarted(container: Container) {
 		unsubscribe = viewState.subscribe(($viewState) => {
 			for (const characterID of Object.keys($viewState)) {
 				const character = $viewState[characterID];
-				let graphics = charactersDisplayObjects[characterID];
-				if (!graphics) {
-					graphics = new Graphics().rect(0, 0, 10, 10).fill(0xff0000);
-					container.addChild(graphics);
-					charactersDisplayObjects[characterID] = graphics;
+				let characterContainer = charactersDisplayObjects[characterID];
+				if (!characterContainer) {
+					characterContainer = new Container();
+					const sprite = new LoadingSprite(Blockie.getURI(character.id));
+					// const graphics = new Graphics().rect(0, 0, 10, 10).fill(0xff0000);
+					// characterContainer.addChild(graphics);
+					characterContainer.addChild(sprite);
+					characterContainer.scale = 10 / 8;
+
+					container.addChild(characterContainer);
+					charactersDisplayObjects[characterID] = characterContainer;
 				}
-				graphics.x = 10 * character.position.x;
-				graphics.y = 10 * character.position.y;
+				characterContainer.x = 10 * character.position.x;
+				characterContainer.y = 10 * character.position.y;
 			}
 			// TODO
 			// const graphics = new Graphics().rect(50, 50, 100, 100).fill(0xff0000);
